8833f40439592079d9a8223bd8f8ce616058c0c5
shreyaaithal <13284481+shreyaaithal@users.noreply.github.com>
2022-05-16 23:55:42 +0530

    Update HTML

--- index copy.php
+++
@@ -1,7 +0,0 @@
-<?php include "templates/header.php"; ?>
-
-<ul>
-	<li><a href="create.php"><strong>Add a Product</strong></a> - add a user</li>
-</ul>
-
-<?php include "templates/footer.php"; ?>
--- index.php
+++ index.php
@@ -1,50 +1,10 @@
-<?php
+<?php include "templates/header.php"; ?>
 
-	echo "<h2>Yo.</h2>";
-	
-	require "config.php";
-	
-    echo "<h2>$host and $username and $password and $db_name and $sslcert</h2>";
+<ul>
+	<li><a href="read.php"><strong>View Catalog</strong></a> - view products</li>
+	<li><a href="insert.php"><strong>Add a Product</strong></a> - add a product</li>
+	<li><a href="update.php"><strong>Update a Product</strong></a> - update a product</li>
+	<li><a href="delete.php"><strong>Remove a Product</strong></a> - update a product</li>
+</ul>
 
-	$conn = mysqli_init();
-
-	echo "<h2>Init complete.</h2>";
-
-    mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-
-	echo "<h2>SSL set complete.</h2>";
-
-    if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL))
-	{
-		echo "<h2>Connection Failed.</h2>";
-		echo mysqli_connect_error();
-	}
-    
-	echo "<h2>Connection Established.</h2>";
-
-    $sql = file_get_contents("schema.sql");
-
-	echo "<h2>$sql</h2>";
-
-    // Run the create table query
-    if(mysqli_query($conn, $sql)){
-        echo "<h2>Table Created.</h2>";
-    }
-    else{
-        echo "<h2>No table Created.</h2>"; 
-    }
-
-    $ProductName = "Pen";
-    $Price = 10.5;
-
-    if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Price) VALUES (?, ?)")) {
-        mysqli_stmt_bind_param($stmt, 'sd', $ProductName, $Price);
-        $status = mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-        echo "<h2>Success Insert.</h2>";
-    }    
-
-    //Close the connection
-    mysqli_close($conn);
-
-?>
+<?php include "templates/footer.php"; ?>
