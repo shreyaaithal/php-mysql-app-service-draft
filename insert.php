68352fc450525a279edf22df2eabe36d76113a04
shreyaaithal <13284481+shreyaaithal@users.noreply.github.com>
2022-05-17 00:05:56 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -10,32 +10,41 @@ if (isset($_POST['submit'])) {
 
     //Establishes the connection
     $conn = mysqli_init();
-    mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
-    if (mysqli_connect_errno($conn)) {
-        echo "<h2>Connection Failed.</h2>";
-    }
-    echo "<h2>Connection Established.</h2>";
+    echo "<h2>Init done.</h2>";
 
-    $sql = file_get_contents("database/schemas.sql");
+    mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+
+	echo "<h2>SSL set complete.</h2>";
+
+    if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL))
+	{
+		echo "<h2>Connection Failed.</h2>";
+		echo mysqli_connect_error();
+	}
+    
+	echo "<h2>Connection Established.</h2>";
+
+    $sql = file_get_contents("schema.sql");
+
+	echo "<h2>$sql</h2>";
 
     // Run the create table query
     if(mysqli_query($conn, $sql)){
         echo "<h2>Table Created.</h2>";
     }
     else{
-        die('Could not create'); 
+        echo "<h2>No table Created.</h2>"; 
     }
 
     $ProductName = $_POST['ProductName'];
     $Price = $_POST['Price'];
 
     if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Price) VALUES (?, ?)")) {
-        mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_price);
+        mysqli_stmt_bind_param($stmt, 'sd', $ProductName, $Price);
         $status = mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
         echo "<h2>Success Insert.</h2>";
-    }    
+    } 
 
     //Close the connection
     mysqli_close($conn);
