a5ae500aa838a18a3a6cbad3c69247e63182c5d4
shreyaaithal <13284481+shreyaaithal@users.noreply.github.com>
2022-05-17 16:10:26 +0530

    Update index page

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php include "templates/header.php"; ?>
 
 <ul>
-	<li><a href="read.php"><strong>View Catalog</strong></a> - view products</li>
-	<li><a href="insert.php"><strong>Add a Product</strong></a> - add a product</li>
-	<li><a href="update.php"><strong>Update a Product</strong></a> - update a product</li>
-	<li><a href="delete.php"><strong>Remove a Product</strong></a> - update a product</li>
+	<li><a href="read.php"><strong>View Catalog</strong></a></li>
+	<li><a href="insert.php"><strong>Add a Product</strong></a></li>
+	<li><a href="update.php"><strong>Update a Product</strong></a></li>
+	<li><a href="delete.php"><strong>Remove a Product</strong></a></li>
 </ul>
 
 <?php include "templates/footer.php"; ?>
